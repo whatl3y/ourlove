22c80c7b0e65bf29f3a2943710e458a9cb124433
Lance Whatley <whatl3y@gmail.com>
2017-07-18 09:41:46 -0400

    try catch in router to pass proper error status and log error

--- src/routes/0.._api_[colon]version_[colon]namespace_[colon]command[star]..get.js
+++ src/routes/0.._api_[colon]version_[colon]namespace_[colon]command[star]..get.js
@@ -6,24 +6,30 @@ import config from '../config'
 const log = bunyan.createLogger(config.logger.options)
 
 export default async function Api(req, res) {
-  const version   = req.params.version
-  const namespace = req.params.namespace
-  const command   = req.params.command
-  const info      = req.params[0]
+  try {
+    const version   = req.params.version
+    const namespace = req.params.namespace
+    const command   = req.params.command
+    const info      = req.params[0]
 
-  switch(namespace) {
-    case 'relationships':
-      switch(command) {
-        case 'get':
-          const relationship  = new Relationships({path: info})
-          const record        = await relationship.getByPath()
-          return res.json({relationship: record})
-      }
-      break
+    switch(namespace) {
+      case 'relationships':
+        switch(command) {
+          case 'get':
+            const relationship  = new Relationships({path: info})
+            const record        = await relationship.getByPath()
+            return res.json({relationship: record})
+        }
+        break
 
-    case 'users':
-      break
+      case 'users':
+        break
+    }
+
+    res.sendStatus(404)
+
+  } catch(err) {
+    log.error(`Error`, err)
+    res.json({error: err}).status(500)
   }
-
-  res.sendStatus(404)
 }
